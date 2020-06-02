95da66777ea0212a7fe2fa90c56216eb4b287f9f
Muhammad Saqib <mohammadsaqib197@gmail.com>
2020-06-02 21:51:38 +0500

    add copyrights

--- resources/views/inc/hm-footer.blade.php
+++ resources/views/inc/hm-footer.blade.php
@@ -35,6 +35,6 @@
             </li>
         </ul>
 
-        <p>&copy; Copyright <span class="f-nor">{{ date('Y') }}</span>, Sweetest Affair.</p>
+        <p>Copyright &copy; <span class="f-nor">{{ date('Y') }}</span>. Powered By <a href="https://dcomm.pk" target="_blank">Dimensions Communications.</a></p>
     </div>
 </footer>
